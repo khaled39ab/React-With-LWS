58acb40507a38972f0d46f0f4dd70849375cd0b0
khaled39ab <khaled39ab@gmail.com>
2022-09-11 23:47:49 +0600

    learn what is react

---
+++ src/class/AboutReact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutReact = () => {
+    return (
+        <div>
+            <h1>First Class: What is React? </h1>
+            <p>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.</p>
+            <p>React is a JavaScript library created by Facebook. <br />
+                React is a User Interface (UI) library. <br />
+                React is a tool for building UI components</p>
+        </div>
+    );
+};
+
+export default AboutReact;
