1652c017357edc06f7e804ef2dbc0d64bd02c073
khaled39ab <khaled39ab@gmail.com>
2022-09-11 23:53:47 +0600

    learn what is react and how it works

--- src/class/AboutReact.js
+++ src/class/AboutReact.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const AboutReact = () => {
     return (
-        <div>
+        <div style={{'border':'3px solid cyan', 'padding': '10px', 'margin': '10px'}}>
             <h1>First Class: What is React? </h1>
             <p>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.</p>
             <p>React is a JavaScript library created by Facebook. <br />
                 React is a User Interface (UI) library. <br />
                 React is a tool for building UI components</p>
+
+            <br />
+            <h1>How React Works</h1>
+            <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
         </div>
     );
 };
