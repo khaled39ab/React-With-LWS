061885b01ee0d01f3140d5fa06f9f6520ef7de1a
khaled39ab <khaled39ab@gmail.com>
2022-09-14 23:50:09 +0600

    react prerequisites

---
+++ src/class/Prerequisites.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Prerequisites = () => {
+    return (
+        <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px' }}>
+            <h2>Prerequisites For Learn React</h2>
+            <ul>
+                <li>Html & Css</li>
+                <li>Modern Javascript</li>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Prerequisites;
