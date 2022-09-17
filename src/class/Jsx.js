145457c9f67fc763579b9cee17824f12dc93992f
khaled39ab <khaled39ab@gmail.com>
2022-09-17 23:49:42 +0600

    react jsx bebefit

--- src/class/Jsx.js
+++ src/class/Jsx.js
@@ -22,6 +22,15 @@ const Jsx = () => {
                 const element = &lt;h1&gt;Welcome to React.&lt;/h1&gt;;
             </p>
 
+            <h2>Why JSX?</h2>
+            <p>
+                React embraces the fact that rendering logic is inherently coupled with other UI logic: how events are handled, how the state changes over time, and how the data is prepared for display.
+
+                Instead of artificially separating technologies by putting markup and logic in separate files, React separates concerns with loosely coupled units called “components” that contain both. We will come back to components in a further section, but if you're not yet comfortable putting markup in JS, this talk might convince you otherwise.
+
+                React doesn't require using JSX, but most people find it helpful as a visual aid when working with UI inside the JavaScript code. It also allows React to show more useful error and warning messages.
+            </p>
+
         </div>
     );
 };
