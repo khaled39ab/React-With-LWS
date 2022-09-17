dec932482e946cb3ef4e4af39d327e1a131484d0
khaled39ab <khaled39ab@gmail.com>
2022-09-17 23:47:07 +0600

    react jsx bebefit

--- src/class/Jsx.js
+++ src/class/Jsx.js
@@ -14,6 +14,14 @@ const Jsx = () => {
             <p>
                 It is simply a syntax extension of JavaScript. It allows us to directly write HTML in React (within JavaScript code). It is easy to create a template using JSX in React, but it is not a simple template language instead it comes with the full power of JavaScript.
             </p>
+            <h2>Benefit of JSX</h2>
+            <p>
+                JSX is faster than normal JavaScript as it performs optimizations while translating to regular JavaScript. Instead of separating the markup and logic in separated files, React uses components for this purpose. We will learn about components in detail in further articles.
+                <br />
+                <strong>Ex. Syntax:</strong> <br />
+                const element = &lt;h1&gt;Welcome to React.&lt;/h1&gt;;
+            </p>
+
         </div>
     );
 };
