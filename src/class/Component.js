a9b29c209ffbee9ccd50a63f3cd27e47d126cbd2
khaled39ab <khaled39ab@gmail.com>
2022-09-18 23:37:13 +0600

    react component

---
+++ src/class/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div>
+            <h1>What is react component?</h1>
+            <p>
+                A Component is one of the core building blocks of React. In other words, we can say that every application you will develop in React will be made up of pieces called components. Components make the task of building UIs much easier
+            </p>
+        </div>
+    );
+};
+
+export default Component;
