998befb626c6c59249337104010eab3ac420239f
khaled39ab <khaled39ab@gmail.com>
2022-09-18 23:45:20 +0600

    change

--- src/class/Jsx.js
+++ src/class/Jsx.js
@@ -14,13 +14,6 @@ const Jsx = () => {
             <p>
                 It is simply a syntax extension of JavaScript. It allows us to directly write HTML in React (within JavaScript code). It is easy to create a template using JSX in React, but it is not a simple template language instead it comes with the full power of JavaScript.
             </p>
-            <h2>Benefit of JSX</h2>
-            <p>
-                JSX is faster than normal JavaScript as it performs optimizations while translating to regular JavaScript. Instead of separating the markup and logic in separated files, React uses components for this purpose. We will learn about components in detail in further articles.
-                <br />
-                <strong>Ex. Syntax:</strong> <br />
-                const element = &lt;h1&gt;Welcome to React.&lt;/h1&gt;;
-            </p>
 
             <h2>Why JSX?</h2>
             <p>
@@ -29,6 +22,14 @@ const Jsx = () => {
                 Instead of artificially separating technologies by putting markup and logic in separate files, React separates concerns with loosely coupled units called “components” that contain both. We will come back to components in a further section, but if you're not yet comfortable putting markup in JS, this talk might convince you otherwise.
 
                 React doesn't require using JSX, but most people find it helpful as a visual aid when working with UI inside the JavaScript code. It also allows React to show more useful error and warning messages.
+            </p>
+
+            <h2>Benefit of JSX</h2>
+            <p>
+                JSX is faster than normal JavaScript as it performs optimizations while translating to regular JavaScript. Instead of separating the markup and logic in separated files, React uses components for this purpose. We will learn about components in detail in further articles.
+                <br />
+                <strong>Ex. Syntax:</strong> <br />
+                const element = &lt;h1&gt;Welcome to React.&lt;/h1&gt;;
             </p>
 
         </div>
