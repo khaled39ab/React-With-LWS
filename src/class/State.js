bdc41c35f27ec1dc0858bed1181726e63a5a6f33
khaled39ab <khaled39ab@gmail.com>
2022-09-21 22:32:38 +0600

    react state

---
+++ src/class/State.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const State = () => {
+    return (
+        <div>
+            <h1>What is state?</h1>
+            <p>
+                React components has a built-in state object.
+                The state object is where you store property values that belongs to the component.
+                When the state object changes, the component re-renders.
+            </p>
+        </div>
+    );
+};
+
+export default State;
