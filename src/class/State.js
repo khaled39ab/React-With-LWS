d9eb8897d2041813bae20ba5cf150bdd0ea030eb
khaled39ab <khaled39ab@gmail.com>
2022-09-24 23:16:59 +0600

    ...

--- src/class/State.js
+++ src/class/State.js
@@ -9,7 +9,7 @@ const State = () => {
                 The state object is where you store property values that belongs to the component.
                 When the state object changes, the component re-renders.
 
-                <br /> <br />
+                <br />
 
                 The state is an instance of React Component Class can be defined as an object of a set of observable properties that control the behavior of the component. In other words, the State of a component is an object that holds some information that may change over the lifetime of the component.
             </p>
@@ -25,14 +25,14 @@ const State = () => {
                         Props are immutable i.e. once set the props cannot be changed, while State is an observable object that is to be used to hold data that may change over time and to control the behavior after each change.
                     </li>
                     <li>
-                        States can be used in Class Components, Functional components with the use of React Hooks (useState and other methods) while Props don’t have this limitation.
+                        States can be used in Class Components, Functional components with the use of React Hooks (useState and other methods) while Props don't have this limitation.
                     </li>
                 </ul>
             </p>
 
             <h2>Conventions of Using State in React</h2>
             <p>
-                State of a component should prevail throughout the lifetime, thus we must first have some initial state, to do so we should define the State in the constructor of the component’s class.
+                State of a component should prevail throughout the lifetime, thus we must first have some initial state, to do so we should define the State in the constructor of the component's class.
             </p>
             <p>
                 State should never be updated explicitly. React uses an observable object as the state that observes what changes are made to the state and helps the component behave accordingly. For example, if we update the state of any component like the following the webpage will not re-render itself because React State will not be able to detect the changes made.
