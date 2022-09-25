112595e3378a06774176a4ca0e54ff2992352d2e
khaled39ab <khaled39ab@gmail.com>
2022-09-26 00:06:45 +0600

    Passing Arguments to Event Handlers example

--- src/class/EventHandling.js
+++ src/class/EventHandling.js
@@ -38,8 +38,8 @@ const EventHandling = () => {
             <h2>Passing Arguments to Event Handlers</h2>
             <p>
                 Inside a loop, it is common to want to pass an extra parameter to an event handler. For example, if id is the row ID, either of the following would work:
-                {/* Example:
-                <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+                Example: (example are comment mood)
+                {/* <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
                 <button onClick={this.deleteRow.bind(this, id)}>Delete Row</button> */}
 
                 The above two lines are equivalent, and use arrow functions and Function.prototype.bind respectively.
