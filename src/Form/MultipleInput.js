8daa753924df3fedc690b01a14194b0dfc670f0b
khaled39ab <khaled39ab@gmail.com>
2022-10-01 19:25:39 +0600

    display setState

--- src/Form/MultipleInput.js
+++ src/Form/MultipleInput.js
@@ -5,7 +5,7 @@ const MultipleInput = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [adress, setAddress] = useState('');
+    const [address, setAddress] = useState('');
     const handleSubmit = (e) => {
         const nameInput = e.target.name.value;
         const emailInput = e.target.email.value;
@@ -22,6 +22,11 @@ const MultipleInput = () => {
             <h1>Handle multiple input</h1>
             <form action="" onSubmit={handleSubmit}>
                 <h4>Input your data</h4>
+                <p>
+                    <strong>Name: {name}</strong> <br />
+                    <strong>Email: {email}</strong> <br />
+                    <strong>Address: {address}</strong>
+                </p>
                 <input name='name' type="text" placeholder='Enter Your Name' /> <br />
                 <input name='email' type="email" placeholder='Enter Your Email' /> <br />
                 <textarea name="address" id="" cols="21" rows="2" placeholder='Enter Your Address'></textarea> <br />
