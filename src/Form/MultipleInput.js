d812e68de0ab6eacd2d913186d4545e6aca37402
khaled39ab <khaled39ab@gmail.com>
2022-10-01 11:49:56 +0600

    submit multiple input from

--- src/Form/MultipleInput.js
+++ src/Form/MultipleInput.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const MultipleInput = () => {
+
+    const handleSubmit = (e) => {
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const address = e.target.address.value;
+        const info ='Name: ' + name + '\n' + 'email: ' + email + '\n' + 'Address: ' + address;
+        alert('Check your info ' + '\n' + info )
+    }
     return (
         <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px', 'textAlign': 'center' }}>
             <h1>Handle multiple input</h1>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <h4>Input your data</h4>
-                <input type="text" placeholder='Enter Your Name' /> <br />
-                <input type="email" placeholder='Enter Your Email' /> <br />
+                <input name='name' type="text" placeholder='Enter Your Name' /> <br />
+                <input name='email' type="email" placeholder='Enter Your Email' /> <br />
                 <textarea name="address" id="" cols="21" rows="2" placeholder='Enter Your Address'></textarea> <br />
                 <input type="submit" value="Submit" />
             </form>
