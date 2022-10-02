11860b8eae133d0c8dd95b61c6e07524f4226bec
khaled39ab <khaled39ab@gmail.com>
2022-10-02 22:34:39 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Class from './class/Class';
 import MultipleInput from './Form/MultipleInput';
 import Signup from './Form/Signup';
 import SingleForm from './Form/SingleForm';
+import Navbar from './page/Navbar';
+
 
 function App() {
   return (
     <div>
-      <Class></Class>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/class" element={<Class></Class>}></Route>
+      </Routes>
       <SingleForm></SingleForm>
       <MultipleInput></MultipleInput>
       <Signup></Signup>
