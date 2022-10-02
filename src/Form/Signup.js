1bd8206bb0ee26ac01f77d8af6a1bd3c1fa006d3
khaled39ab <khaled39ab@gmail.com>
2022-10-02 14:13:02 +0600

    form validation

--- src/Form/Signup.js
+++ src/Form/Signup.js
@@ -7,10 +7,9 @@ const Signup = () => {
     const [lname, setLname] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
 
     /* const handleSubmit = (e) => {
-        e.preventDefault();
-
         const fullName = e.target.fname.value + " " + e.target.lname.value;
 
         setName(fullName);
@@ -19,10 +18,23 @@ const Signup = () => {
         e.target.reset();
     } */
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!(fname.match(/^[a-zA-Z]+$/))){
+            setError('First name is not valid')
+        }
+        if(!(lname.match(/^[a-zA-Z]+$/))){
+            setError('Last name is not valid')
+        }
+        
+        e.target.reset();
+    }
+    
     return (
         <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px', 'textAlign': 'center' }}>
             <h1>SignUp Page</h1>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <input type="text" onChange={(e) =>setFname(e.target.value)} placeholder='Enter First Name' name='fname' required /> <br />
                 <input type="text" onChange={(e) =>setLname(e.target.value)} placeholder='Enter Last Name' name='lname' required /> <br />
                 <input type="email" onChange={(e) =>setEmail(e.target.value)} placeholder='Enter Your Email' name='email' required /> <br />
@@ -33,6 +45,7 @@ const Signup = () => {
             <p>Last Name: {lname}</p>
             <p>Email: {email}</p>
             <p>Phone Number: {phone}</p>
+            <p style={{'color': 'red'}}>{error}</p>
         </div>
     );
 };
