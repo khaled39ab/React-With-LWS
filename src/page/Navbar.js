6268e5bba6b74cb6298f105a098315ed82693b81
khaled39ab <khaled39ab@gmail.com>
2022-10-10 21:23:09 +0600

    navbar css

--- src/page/Navbar.js
+++ src/page/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/class'}>Class</Link>
-            <Link to={'/form'}>Form</Link>
-        </>
+        <div className='navbar-container'>
+            <div className='icon'>
+                <h1>Learn React</h1>
+            </div>
+            <div className='nav'>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/class'}>Class</Link>
+                <Link to={'/form'}>Form</Link>
+            </div>
+        </div>
     );
 };
 
