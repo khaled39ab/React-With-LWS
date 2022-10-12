7f004c0b431aa49a875f589933e2f1e9e68247ba
khaled39ab <khaled39ab@gmail.com>
2022-10-12 20:51:02 +0600

    get data with axios

--- src/HttpReq/Country.js
+++ src/HttpReq/Country.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 import React from 'react';
-import { useEffect } from 'react';
 import { useState } from 'react';
 import CountryInfo from './CountryInfo';
 
 const Country = () => {
     const [countries, setCountries] = useState([]);
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then (res => res.json())
-        .then (data => setCountries(data))
-    },[])
-/* 
+
     axios.get('https://restcountries.com/v3.1/all')
         .then(res => {
             setCountries(res.data);
@@ -19,10 +13,10 @@ const Country = () => {
         .catch(err => {
             console.log(err);
         });
-       */  
+        
     return (
         <div>
-            <h1 style={{color:'blueviolet'}}>Introduce Country</h1>
+            <h1 style={{color:'blue'}}>Introduce Country</h1>
             <div>
                 {
                     countries.map(country => 
