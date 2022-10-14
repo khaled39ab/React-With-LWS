609c26eca9dabfe77238ea4136e9d5c4fd9e4ee1
khaled39ab <khaled39ab@gmail.com>
2022-10-14 23:07:20 +0600

    get quotes component

--- src/HttpReq/More.js
+++ src/HttpReq/More.js
@@ -1,11 +1,31 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 import HttpNavbar from './HttpNavbar';
 
 const More = () => {
+    const url = "https://api.quotable.io/random";
+    const [quote, setQuote] = useState('')
+
+    axios.get(url)
+        .then(res => {
+            setQuote(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    const {content, author} = quote;
     return (
         <div>
             <HttpNavbar></HttpNavbar>
-            <h1>Http request more</h1>
+            <h1>Best Quote</h1>
+            {
+                // quote.map( quoteCon =>{
+
+                // })
+            }
+            <p>{content}</p>
         </div>
     );
 };
