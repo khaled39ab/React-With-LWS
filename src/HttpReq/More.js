bd539232efede1ec18bb9e568c7e24dc22cdbb0c
khaled39ab <khaled39ab@gmail.com>
2022-10-15 22:03:02 +0600

    get quote from server done

--- src/HttpReq/More.js
+++ src/HttpReq/More.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import HttpNavbar from './HttpNavbar';
 
@@ -7,25 +8,34 @@ const More = () => {
     const url = "https://api.quotable.io/random";
     const [quote, setQuote] = useState('')
 
-    axios.get(url)
-        .then(res => {
-            setQuote(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    const getQuote = () => {
+        axios.get(url)
+            .then(res => {
+                setQuote(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-    const {content, author} = quote;
+    useEffect(() => {
+        getQuote()
+    }, [])
+
+    const getNewQuote = () => {
+        getQuote()
+    }
+
+    const { content, author } = quote;
     return (
         <div>
             <HttpNavbar></HttpNavbar>
-            <h1>Best Quote</h1>
-            {
-                // quote.map( quoteCon =>{
-
-                // })
-            }
-            <p>{content}</p>
+            <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px' }}>
+                <h1>Best Quote</h1>
+                <h4><span style={{color: 'red'}}>Author:</span> {author}</h4>
+                <h5><span style={{color: 'red'}}>Quote:</span> {content}</h5>
+                <button onClick={getNewQuote}>Next</button>
+            </div>
         </div>
     );
 };
