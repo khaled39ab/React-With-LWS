d9f66e7a723f08e063ab4f9ef0d44c4dbb14bb2a
khaled39ab <khaled39ab@gmail.com>
2022-10-27 23:19:49 +0600

    added more data

--- src/page/cards/CardGroup.js
+++ src/page/cards/CardGroup.js
@@ -5,20 +5,30 @@ import './CardGroup.css';
 import Data from './../../Data.json';
 
 const CardGroup = () => {
-    const frameworks = Data.map((dt, index) => <Card
-        key={index}
-        fmName={dt.name}
-        fmDesc={dt.desc}
-        fmImg={dt.img}
-    />)
-    
+    /* 
+        const frameworks = Data.map((dt, index) => <Card
+            key={index}
+            fmName={dt.name}
+            fmDesc={dt.desc}
+            fmImg={dt.img}
+        />)
+    */
+
     return (
         <div>
             <OthersNavbar></OthersNavbar>
             <div>
                 <h1 className='heading'>Programming Language Framework</h1>
                 <div className='cardGroup'>
-                    {frameworks}
+                    {
+                        Data.map((dt, index) => <Card
+                            key={index}
+                            fmName={dt.name}
+                            fmDesc={dt.desc}
+                            fmImg={dt.img}
+                        />
+                        )
+                    }
                 </div>
             </div>
         </div>
