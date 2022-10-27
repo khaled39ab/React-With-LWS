f43a5c28e61dde6fdc07f981ff6450f82a47679c
khaled39ab <khaled39ab@gmail.com>
2022-10-27 23:30:25 +0600

    added more data

--- src/page/cards/Card.js
+++ src/page/cards/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 const Card = (props) => {
     const { fmName, fmDesc, fmImg } = props;
-    console.log( fmImg);
+    console.log(fmImg);
     return (
         <div>
             <div className='card-container'>
@@ -13,7 +13,7 @@ const Card = (props) => {
                 <div className='card-desc'>
                     <h2 className='card-title'>{fmName}</h2>
                     <p>{fmDesc}</p>
-                    <button className='card-btn'>Next</button>
+                    <a href="https://www.monocubed.com/blog/most-popular-web-frameworks/" target="blank"><button className='card-btn'>Go to Web</button></a>
                 </div>
             </div>
         </div>
