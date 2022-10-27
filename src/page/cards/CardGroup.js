65d5920ae6419eb6ddc8341278bca2ba79509b9b
khaled39ab <khaled39ab@gmail.com>
2022-10-27 22:18:01 +0600

    create card group

---
+++ src/page/cards/CardGroup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CardGroup.css';
+
+const CardGroup = () => {
+    return (
+        <div>
+            <h1>Programming Language Framework</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default CardGroup;
