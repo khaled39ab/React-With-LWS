3fdbda1f6e69b857616029e571ef52e690016044
khaled39ab <khaled39ab@gmail.com>
2022-10-29 22:55:46 +0600

    create mini project with intuition of react

---
+++ src/assignment/AllAssignment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Assignment1 from './Assignment1';
+
+const AllAssignment = () => {
+    return (
+        <div>
+            <Assignment1></Assignment1>
+        </div>
+    );
+};
+
+export default AllAssignment;
---
+++ src/assignment/Assignment1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Assignment1 = () => {
+    return (
+        <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px' }}>
+            <h1>React Assignment - 1 : Product Listing App</h1>
+        </div>
+    );
+};
+
+export default Assignment1;
