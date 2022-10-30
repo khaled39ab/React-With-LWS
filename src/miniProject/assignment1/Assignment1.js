da5d504bf2454fca7700e6bbe9ca9a1697ff80bb
khaled39ab <khaled39ab@gmail.com>
2022-10-30 22:04:54 +0600

    change a bit

--- src/miniProject/assignment1/Assignment1.js
+++ src/miniProject/assignment1/Assignment1.js
@@ -62,7 +62,7 @@ const Assignment1 = () => {
             price: 695,
             description:
                 "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-            category: 'jewelery',
+            category: 'jewelry',
             image: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
             rating: {
                 rate: 4.6,
@@ -75,7 +75,7 @@ const Assignment1 = () => {
             price: 168,
             description:
                 'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.',
-            category: 'jewelery',
+            category: 'jewelry',
             image: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg',
             rating: {
                 rate: 3.9,
