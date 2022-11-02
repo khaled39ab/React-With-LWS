629694f321d9f24b7fb99636a93c7d3792a3ffc2
khaled39ab <khaled39ab@gmail.com>
2022-11-02 21:47:23 +0600

    conditional rendering with ternary

--- src/conditional-rendering/Conditional_Rendering.js
+++ src/conditional-rendering/Conditional_Rendering.js
@@ -13,12 +13,21 @@ class Conditional_Rendering extends Component {
     }
 
     render() {
-
+/* 
         if (this.state.isLoggedIn) {
             return <Permission></Permission>
         } else {
             return <Login></Login>
-        } 
+        }
+         */
+
+        const {isLoggedIn} = this.state;
+
+        return ( 
+            <div>
+                {isLoggedIn ? <Permission></Permission>:<Login></Login>}
+            </div>
+        ) 
         
     }
 }
