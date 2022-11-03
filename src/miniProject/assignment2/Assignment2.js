0b8f247d5d992e47e73b287e71f1334926427161
khaled39ab <khaled39ab@gmail.com>
2022-11-03 15:56:58 +0600

    change some style

--- src/miniProject/assignment2/Assignment2.js
+++ src/miniProject/assignment2/Assignment2.js
@@ -39,10 +39,10 @@ const Assignment2 = () => {
                     <h1 className="counter__title">Counter App</h1>
                     <div className="card center">
                         <h2 className="card__title">Count : 0</h2>
-                        <div className="card__btns">
-                            <button className="btn card__btn">+</button>
-                            <button className="btn card__btn">-</button>
-                            <button className="btn card__btn">0</button>
+                        <div className="card__btn">
+                            <button className="btn2 card__btn">+</button>
+                            <button className="btn2 card__btn">-</button>
+                            <button className="btn2 card__btn">0</button>
                         </div>
                     </div>
                 </div>
