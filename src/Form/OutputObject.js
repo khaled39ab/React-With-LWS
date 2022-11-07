49170ae1fc9c4e0bfb2f2438cee54419cfc20978
khaled39ab <khaled39ab@gmail.com>
2022-11-07 19:11:42 +0600

    send data as objecct

--- src/Form/OutputObject.js
+++ src/Form/OutputObject.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 
 const OutputObject = () => {
+    const [user, setUser] = useState({ name: '', email: '', phone: '', password: '' });
+    const { name, email, phone, password } = user;
+
+    const handleName = e => {
+        setUser({ name: e.target.value, email, phone, password })
+    }
+
+    const handleEmail = e => {
+        setUser({ name, email: e.target.value, phone, password })
+    }
+
+    const handlePhone = e => {
+        setUser({ name, email, phone: e.target.value, password })
+    }
+
+    const handlePassword = e => {
+        setUser({ name, email, phone, password: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user)
+    }
 
     return (
         <div style={{ 'border': '4px solid cyan', 'padding': '10px', 'margin': '10px', 'textAlign': 'center' }}>
             <h1>Send data as object</h1>
-            <form action="">
-                <input type="text" placeholder='Enter First Name' name='fname' required /> <br />
-                <input type="text" placeholder='Enter Last Name' name='lname' required /> <br />
-                <input type="email" placeholder='Enter Your Email' name='email' required /> <br />
-                <input type="number"  placeholder='Enter Phone Number' name='phone' required /> <br />
+            <form action="" onClick={handleSubmit}>
+                <input type="text" onChange={handleName} placeholder='Enter Your Name' name='name' required /> <br />
+                <input type="email" onChange={handleEmail} placeholder='Enter Your Email' name='email' required /> <br />
+                <input type="number" onChange={handlePhone} placeholder='Enter Phone Number' name='phone' required /> <br />
+                <input type="password" onChange={handlePassword} placeholder='Enter Your Password' name='password' required /> <br />
                 <input type="submit" value="SignUp Now" />
             </form>
         </div>
