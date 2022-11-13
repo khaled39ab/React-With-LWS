ea7c732205a344b7f4fa781a696767de9a4e106a
khaled39ab <khaled39ab@gmail.com>
2022-11-13 22:35:19 +0600

    handle input and submit button

--- src/page/todosApp/NewTodo.js
+++ src/page/todosApp/NewTodo.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 
 const NewTodo = () => {
+    const [todo, setTodo] = useState('');
+
+    const handleInput = e =>{
+        setTodo(e.target.value)
+    }
+
+    const handleTodo = (e) => {
+        e.preventDefault();
+        console.log(todo)
+        e.target.reset()
+    }
+    
     return (
         <div>
-            <form action="">
+            <form onSubmit={handleTodo}>
                 <label htmlFor="newTodo">New Todo: </label>
-                <input type="text" />
+                <input type="text" name='todo' onChange={handleInput}/>
                 <button>Add Todo</button>
             </form>
         </div>
