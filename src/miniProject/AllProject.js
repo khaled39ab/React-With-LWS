77a992977ad95b9234d96feb65f1f7a09dae694e
khaled39ab <khaled39ab@gmail.com>
2022-11-15 21:57:24 +0600

    main todo app creating

--- src/miniProject/AllProject.js
+++ src/miniProject/AllProject.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OthersNavbar from '../page/OthersNavbar';
 import Assignment1 from './assignment1/Assignment1';
 import Assignment2 from './assignment2/Assignment2';
+import TodoHome from './todoApp/TodoHome';
 
 const AllProject = () => {
     return (
@@ -9,6 +10,7 @@ const AllProject = () => {
             <OthersNavbar></OthersNavbar>
             <Assignment1></Assignment1>
             <Assignment2></Assignment2>
+            <TodoHome></TodoHome>
         </div>
     );
 };
---
+++ src/miniProject/todoApp/TodoHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoHome = () => {
+    return (
+        <div style={{ 'border': '4px solid cyan', 'padding': '20px', 'margin': '10px' }}>
+            <h1>A Todo App</h1>
+        </div>
+    );
+};
+
+export default TodoHome;
