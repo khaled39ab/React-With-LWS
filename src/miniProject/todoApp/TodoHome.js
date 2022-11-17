5f21ae48ad893dbef2efa7473ecb5f93bc2090d6
khaled39ab <khaled39ab@gmail.com>
2022-11-17 23:57:40 +0600

    new project

--- src/miniProject/todoApp/TodoHome.js
+++ src/miniProject/todoApp/TodoHome.js
@@ -3,7 +3,14 @@ import React from 'react';
 const TodoHome = () => {
     return (
         <div style={{ 'border': '4px solid cyan', 'padding': '20px', 'margin': '10px' }}>
-            <h1>A Todo App</h1>
+            <div>
+                <h1>React Todo App</h1> <br />
+            </div>
+            <hr />
+            <hr />
+            <div>
+
+            </div>
         </div>
     );
 };
