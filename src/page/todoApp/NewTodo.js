99ab797c5211cc21c64e88148297a47430d21b19
khaled39ab <khaled39ab@gmail.com>
2022-11-20 22:57:15 +0600

    handle add todo btn

--- src/page/todoApp/NewTodo.js
+++ src/page/todoApp/NewTodo.js
@@ -3,27 +3,31 @@ import './NewTodo.css'
 import { useState } from 'react';
 
 const NewTodo = (props) => {
-    const [todo, setTodo] = useState('');
+    const [todo, setTodo] = useState({ title: '', desc: '' });
+    const { title, desc } = todo;
 
-    const handleInput = e => {
-        setTodo(e.target.value)
+    const handleChange = e => {
+        const name = e.target.name;
+        setTodo(oldValue => {
+            return { ...oldValue, [name]: e.target.value }
+        })
     }
 
     const handleTodo = (e) => {
         e.preventDefault();
-        props.onTodo(todo)
-        e.target.reset()
+        console.log(todo);
+        setTodo({ title: '', desc: '' })
     }
 
     return (
         <form className='form' onSubmit={handleTodo}>
             <div className='form-field'>
                 <label htmlFor="title">Title: </label>
-                <input type="text" name='title' id='title' onChange={handleInput} />
+                <input type="text" name='title' id='title' value={title} onChange={handleChange} />
             </div>
             <div className='form-field'>
                 <label htmlFor="desc">Description:</label>
-                <textarea type="text" name="desc" id="desc" />
+                <textarea type="text" name="desc" id="desc" value={desc} onChange={handleChange} />
             </div>
             <button className='add-btn'>Add Todo</button>
         </form>
