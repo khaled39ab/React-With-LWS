e3ccbe6946be4785a289993df76ae21cc30ee428
khaled39ab <khaled39ab@gmail.com>
2022-11-24 22:51:09 +0600

    new assignment file ready

--- src/miniProject/AllProject.js
+++ src/miniProject/AllProject.js
@@ -3,6 +3,7 @@ import OthersNavbar from '../page/OthersNavbar';
 import Assignment1 from './assignment1/Assignment1';
 import Assignment2 from './assignment2/Assignment2';
 import Assignment3 from './assignment3/Assignment3';
+import Assignment4 from './assignment4/Assignment4';
 
 const AllProject = () => {
     return (
@@ -11,6 +12,7 @@ const AllProject = () => {
             <Assignment1></Assignment1>
             <Assignment2></Assignment2>
             <Assignment3></Assignment3>
+            <Assignment4></Assignment4>
         </div>
     );
 };
--- src/miniProject/assignment4/Assignment4.js
+++ src/miniProject/assignment4/Assignment4.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Assignment4 = () => {
     return (
-        <div>
-            
+        <div style={{ 'border': '4px solid cyan', 'padding': '20px', 'margin': '10px' }}>
+            <div>
+                <h1>React Assignment - 4 : User Management App</h1> <br />
+
+                <h4>Testing students skills on</h4>
+
+                <ul>
+                    <li>useEffect() hook</li>
+                    <li>error handling when loading data</li>
+                    <li>Custom hook</li>
+                </ul>
+
+                <br />
+
+                <h4> <strong>Assignment steps:</strong></h4>
+
+                <ol>
+                    <li> step1 : declare three states : users, isLoading, error </li>
+                    <li>step2 : use useEffect for fetching the data including updating isLoading and error states </li>
+                    <li>step3 : pass the users data to Users component</li>
+
+                    <li>finally check the project demo and try to match your one as much as possible. <a href="https://users-mgt-app.netlify.app/" target="blank">Link</a></li>
+                </ol>
+            </div>
+            <hr />
+            <hr />
+
         </div>
     );
 };
