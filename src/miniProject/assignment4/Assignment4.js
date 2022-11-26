1cd324b12a12b97712c5f3a171265fd3d4ac7b8f
khaled39ab <khaled39ab@gmail.com>
2022-11-26 23:48:58 +0600

    custom hook is working

--- src/miniProject/assignment4/Assignment4.js
+++ src/miniProject/assignment4/Assignment4.js
@@ -4,34 +4,36 @@ import useFetch from './useFetch';
 import Users4 from './Users4';
 
 const Assignment4 = () => {
-    const [users, setUsers] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null)
+    // const [users, setUsers] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError] = useState(null)
+
+
+    // useEffect(() => {
+    //     // setTimeout(()=>{
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //         .then(res => {
+    //             if (!res.ok) {
+    //                 throw Error('Fetching is not successful...!')
+    //             }
+    //             else {
+    //                 return res.json()
+    //             }
+    //         })
+    //         .then((data) => {
+    //             setUsers(data)
+    //             setIsLoading(false)
+    //             setError(null)
+    //         })
+    //         .catch(err => {
+    //             setError(err.message)
+    //             setIsLoading(false)
+    //         })
+    //     // }, 2000)
+    // }, [])
+    const { users, isLoading, error } = useFetch("https://jsonplaceholder.typicode.com/users")
 
     const loadingMessage = <p>Users is Loading...</p>
-
-    useEffect(() => {
-        // setTimeout(()=>{
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Fetching is not successful...!')
-                }
-                else {
-                    return res.json()
-                }
-            })
-            .then((data) => {
-                setUsers(data)
-                setIsLoading(false)
-                setError(null)
-            })
-            .catch(err => {
-                setError(err.message)
-                setIsLoading(false)
-            })
-        // }, 2000)
-    }, [])
 
     return (
         <div style={{ 'border': '4px solid cyan', 'padding': '20px', 'margin': '10px' }}>
@@ -68,7 +70,7 @@ const Assignment4 = () => {
                     {/* {error && <p>{error}</p>} */}
                     {/* step3 : pass the users data to Users component  */}
                     {
-                        error && <p style={{color: 'red'}}>{error}</p>
+                        error && <p style={{ color: 'red' }}>{error}</p>
                     }
                     {
                         isLoading && loadingMessage
--- src/miniProject/assignment4/useFetch.js
+++ src/miniProject/assignment4/useFetch.js
@@ -1,16 +1,33 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-const useFetch = () => {
-    const [usersData, setUsersData] = useState([]);
-    
+const useFetch = (url) => {
+    const [users, setUsers] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null)
+
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then(data => setUsersData(data))
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Fetching is not successful...!')
+                }
+                else {
+                    return res.json()
+                }
+            })
+            .then((data) => {
+                setUsers(data)
+                setIsLoading(false)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err.message)
+                setIsLoading(false)
+            })
     }, [])
-    
-    return usersData;
+
+    return { users, isLoading, error };
 };
 
 export default useFetch;
