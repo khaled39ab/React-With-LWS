e12fd2236762148c1feb5829dd760adbc2cbf654
khaled39ab <khaled39ab@gmail.com>
2022-11-28 17:51:04 +0600

    added new book

--- src/page/reducer/UseReducer.js
+++ src/page/reducer/UseReducer.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import OthersNavbar from '../OthersNavbar';
 
+const bookList = [
+    { id: 101, name: 'Fik-hus Sirah' },
+    { id: 102, name: 'Rain Drops Sirah' },
+    { id: 103, name: 'Roufur Rahim' },
+    { id: 104, name: 'Ar-Rahikul Maktum' },
+    { id: 105, name: 'Jemon Chilen Tini' }
+]
+
 const UseReducer = () => {
+    const [books, setBooks] = useState(bookList);
+    const [bookName, setBookName] = useState('')
+
+    const handleAddBook = e => {
+        e.preventDefault();
+        setBooks((prevValue)=>{
+            const newBook = {id:new Date().getTime(), name: bookName};
+            return [...prevValue, newBook]
+        });
+        setBookName('')
+    }
+
     return (
         <>
             <OthersNavbar />
-            <h1>Learn Reducer</h1>
+            <div style={{ textAlign: 'center' }}>
+                <h1>Book List</h1>
+                <form onSubmit={handleAddBook}>
+                    <input
+                        type="text"
+                        placeholder='Add New Book'
+                        defaultValue={bookName}
+                        onChange={(e) => setBookName(e.target.value)}
+                    />
+                    <button>Add Book</button>
+                </form>
+
+                {
+                    books.map(book => {
+                        const { id, name } = book;
+                        return <li key={id}>{name}</li>
+                    })
+                }
+            </div>
         </>
     );
 };
