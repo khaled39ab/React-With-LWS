3866d6d17214642ef7f452f9a9eb3daa52e5e4a7
khaled39ab <khaled39ab@gmail.com>
2022-11-30 21:09:39 +0600

    useContext is done

--- src/page/hook/useContext/Context3.js
+++ src/page/hook/useContext/Context3.js
@@ -4,11 +4,11 @@ import Context4 from './Context4';
 import { DataContext } from './DataContext';
 
 const Context3 = () => {
-    const {text} = useContext(DataContext)
+    const { text } = useContext(DataContext)
     return (
         <div>
             <Context4 />
-            <h3>{text}</h3>
+            <h3>I am child 3 component and I got a message from "{text}"</h3>
         </div>
     );
 };
--- src/page/hook/useContext/Context4.js
+++ src/page/hook/useContext/Context4.js
@@ -3,12 +3,13 @@ import { useContext } from 'react';
 import { DataContext } from './DataContext';
 
 const Context4 = () => {
-    const {data} = useContext(DataContext)
+    const { data, text } = useContext(DataContext)
     const { id, name } = data;
     return (
         <div>
             <h3>{id}</h3>
             <h3>{name}</h3>
+            <h3>I am child 4 component and got message from "{text}"</h3>
         </div>
     );
 };
