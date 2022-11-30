27f1008315a03e9929501b41e8de857526a24903
khaled39ab <khaled39ab@gmail.com>
2022-11-30 20:23:29 +0600

    write rules

--- src/page/hook/useContext/DataContext.js
+++ src/page/hook/useContext/DataContext.js
@@ -1,3 +1,7 @@
 import React from "react";
 
 export const DataContext = React.createContext();
+
+// step-1: create a react context with React.createContext() method
+// step-1: wrap parent component with created context component.provider
+// step-3: state access with useContext in child component
