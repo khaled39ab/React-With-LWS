e528758f174f5e8cae04243bb2b7501ae2f54ad5
khaled39ab <khaled39ab@gmail.com>
2022-12-02 23:32:26 +0600

    truncate paragraph to create dynamic route

--- src/page/dynamic-routing/Blogs.js
+++ src/page/dynamic-routing/Blogs.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Blogs = ({ blog }) => {
 
+    const sliceStr = (str, num) => {
+        if (str.length > num) {
+            return str.slice(0, num) + '...'
+        }
+        else {
+            return str;
+        }
+    }
+
     return (
         <article style={{ margin: '20px' }}>
             <h3>{blog.title}</h3>
-            <p>{blog.desc}</p>
+            <p>{sliceStr(blog.desc, 100)}</p>
         </article>
     );
 };
