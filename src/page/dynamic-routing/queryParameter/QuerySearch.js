5e59381c9c40e4be44223a4179eae4860e970125
khaled39ab <khaled39ab@gmail.com>
2022-12-03 23:19:21 +0600

    create a form for query search

--- src/page/dynamic-routing/queryParameter/QuerySearch.js
+++ src/page/dynamic-routing/queryParameter/QuerySearch.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import DynamicNavbar from '../DynamicNavbar';
 
 const QuerySearch = () => {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <DynamicNavbar/>
-            <div>
+            <DynamicNavbar />
+            <div style={{ textAlign: 'center' }}>
                 <h1>Query Search as User</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder='Name'
+                        onChange={(e) => setName(e.target.value)}
+                    /> <br />
+                    <input
+                        type="text"
+                        placeholder='Age'
+                        onChange={(e) => setAge(e.target.value)}
+                    /> <br />
+                    <button type='submit'>Search User</button>
+                </form>
             </div>
         </div>
     );
