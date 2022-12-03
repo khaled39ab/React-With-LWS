38d78eb50199d72e1b119711317e270d3ddef338
khaled39ab <khaled39ab@gmail.com>
2022-12-03 22:56:40 +0600

    dynamic routing with useLocation

--- src/page/dynamic-routing/Blog.js
+++ src/page/dynamic-routing/Blog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Blog = ({ blog }) => {
+    const {id, title, desc} = blog;
+
     const sliceStr = (str, num) => {
         if (str.length > num) {
             return str.slice(0, num) + '...'
@@ -12,8 +14,8 @@ const Blog = ({ blog }) => {
     }
     return (
         <article style={{ margin: '20px' }}>
-            <h3>{blog.title}</h3>
-            <p>{sliceStr(blog.desc, 100)} <Link to={blog.title}>Learn More</Link></p>
+            <h3>{title}</h3>
+            <p>{sliceStr(desc, 100)} <Link to={blog.title} state={{desc, title}}>Learn More</Link></p>
 
         </article>
     );
--- src/page/dynamic-routing/Details.js
+++ src/page/dynamic-routing/Details.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { blogsData } from './blogsData';
 import DynamicNavbar from './DynamicNavbar';
 
 const Details = () => {
+    /* 
     const { title } = useParams();
     const [details, setDetails] = useState('');
-
     useEffect(() => {
         const body = blogsData.filter(blog => blog.title === title)
         setDetails(body[0].desc)
     }, [title])
+     */
+    const location = useLocation();
 
     return (
         <div>
             <DynamicNavbar />
 
             <div>
-                <h1>About: {title}</h1>
-                <p>{details}</p>
+                <h1>About: {location.state.title}</h1>
+                <p>{location.state.desc}</p>
             </div>
         </div>
     );
